717499475a1e463fae45501731fd1dd812cc7422
tryc0ding <166061609+tryc0ding@users.noreply.github.com>
2024-07-22 10:14:18 +0200

    added voucher-widget.html into index.html via insert

--- landingpage/views.py
+++ landingpage/views.py
@@ -1,9 +1,13 @@
 from django.template.response import TemplateResponse
-
+from django.shortcuts import render
 
 def index(request):
     response = TemplateResponse(request, "index.html", {})
     return response
+
 def impressum(request):
     response = TemplateResponse(request, "impressum.html", {})
     return response
+
+def index(request):
+    return render(request, "index.html")
